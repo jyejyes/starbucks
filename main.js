99954fee9cb773ee6a670fff8ad94055e7d76a82
jyejyes <p_jihye97@naver.com>
2022-01-19 00:55:10 +0900

    second-page make

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let slide_text= document.querySelector('.left-text');
-
+let slide_menus= document.querySelector('.new-menus');
 window.addEventListener('scroll', ()=>{
     let scrollY_position= window.scrollY;
     
     if(scrollY_position > 200){
-        slide_text.style.animation='disapper 1.5s ease-out forwards';
+        slide_text.style.animation='disappear 1.5s ease-out forwards';
     }else{
-        slide_text.style.animation='apper 1.5s ease-out';
+        slide_text.style.animation='appear 1.5s ease-out';
+    }
+
+    if(scrollY_position > 350){
+        slide_menus.style.animation='menus-appear 1.5s ease-out';
+    }else{
+        slide_menus.style.animation='menus-disappear 1s ease-out forwards';
     }
 })
