0c0033bffa2772b03bba24897fe95029511c0b19
jyejyes <p_jihye97@naver.com>
2022-01-31 14:25:03 +0900

    third commit:hover

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 let slide_text= document.querySelector('.left-text');
 let slide_menus= document.querySelector('.new-menus');
+let coffeebtn= document.querySelector('.coffeebtn');
+let noncoffeebtn= document.querySelector('.non-coffeebtn');
+let storebtn= document.querySelector('.storebtn');
+let coffee= document.querySelector('.coffee');
+let non_coffee=document.querySelector('.non-coffee');
+let store= document.querySelector('.store');
+
+coffeebtn.addEventListener('mouseout', ()=>{
+    coffee.style.display="none";   
+})
+coffeebtn.addEventListener('mouseover', ()=>{
+    coffee.style.display='flex';
+})
+noncoffeebtn.addEventListener('mouseout', ()=>{
+    non_coffee.style.display="none";   
+})
+noncoffeebtn.addEventListener('mouseover', ()=>{
+    non_coffee.style.display='flex';
+})
+storebtn.addEventListener('mouseout', ()=>{
+    store.style.display="none";   
+})
+storebtn.addEventListener('mouseover', ()=>{
+    store.style.display='flex';
+})
+
+// 스크롤 시 글자, 그림 슬라이드 되는
 window.addEventListener('scroll', ()=>{
     let scrollY_position= window.scrollY;
     
